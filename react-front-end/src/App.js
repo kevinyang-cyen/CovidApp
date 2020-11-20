9ace04c71b6f8220785a63079867bb4cbde11af0
Kevin Yang <kevinyang.cyen@gmail.com>
2020-11-20 14:58:23 -0800

    more elephantsql configuration

--- react-front-end/src/App.js
+++ react-front-end/src/App.js
@@ -12,11 +12,13 @@ class App extends Component {
   // for CORS problems, add this in front
   // https://cors-anywhere.herokuapp.com/
   fetchData = () => {
-    axios.get('https://health-products.canada.ca/api/clinical-trial/drugproduct/?lang=en&type=json') // You can simply make your requests to "/api/whatever you want"
+    axios.get('https://health-products.canada.ca/api/clinical-trial/protocol/?lang=en&type=json') // You can simply make your requests to "/api/whatever you want"
     .then((response) => {
       // handle success
-      console.log(response.data) // The entire response from the Rails API
-
+      // console.log(response.data) // The entire response from the Rails API
+      let listOfResponses = response.data
+      let covidResponses = listOfResponses.filter(item => item.medConditionList[0].med_condition_id == 1028);
+      console.log(covidResponses);
       // console.log(response.data.message) // Just the message
       this.setState({
         message: response.data
