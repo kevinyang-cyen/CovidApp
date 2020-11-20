3d484e4752529e9cd9877adff6961e3c2e222956
Kevin Yang <kevinyang.cyen@gmail.com>
2020-11-20 10:39:33 -0800

    took out unneccesary code

--- express-back-end/server.js
+++ express-back-end/server.js
@@ -19,25 +19,4 @@ App.listen(PORT, () => {
   console.log(`Express seems to be listening on port ${PORT} so that's pretty good 👍`);
 });
 
-// ElephantSQL Setup
-var pg = require('pg');
-
-//or native libpq bindings
-//var pg = require('pg').native
-
-var conString = process.env['POSTGRESURL'] //Can be found in the Details page
-var client = new pg.Client(conString);
-client.connect(function(err) {
-  if(err) {
-    return console.error('could not connect to postgres', err);
-  }
-  client.query('CREATE TABLE users (id SERIAL PRIMARY KEY NOT NULL, name VARCHAR(255) NOT NULL)', function(err, result) {
-    if(err) {
-      return console.error('error running query', err);
-    }
-    console.log(result.rows[0]);
-    // >> output: 2018-08-23T14:02:57.117Z
-    client.end();
-  });
-});
 
