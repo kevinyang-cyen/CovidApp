d3c7e3a79bcf4f3de17409eb0e16b91c1aca7b9b
Kevin Yang <kevinyang.cyen@gmail.com>
2020-11-19 16:38:41 -0800

    elephantsql query test

--- express-back-end/server.js
+++ express-back-end/server.js
@@ -2,6 +2,7 @@ const Express = require('express');
 const App = Express();
 const BodyParser = require('body-parser');
 const PORT = 8080;
+require('dotenv').config('/.env');
 
 // Express Configuration
 App.use(BodyParser.urlencoded({ extended: false }));
@@ -18,4 +19,25 @@ App.listen(PORT, () => {
   console.log(`Express seems to be listening on port ${PORT} so that's pretty good 👍`);
 });
 
+// ElephantSQL Setup
+var pg = require('pg');
+
+//or native libpq bindings
+//var pg = require('pg').native
+
+var conString = process.env['POSTGRESURL'] //Can be found in the Details page
+var client = new pg.Client(conString);
+client.connect(function(err) {
+  if(err) {
+    return console.error('could not connect to postgres', err);
+  }
+  client.query('CREATE TABLE users (id SERIAL PRIMARY KEY NOT NULL, name VARCHAR(255) NOT NULL)', function(err, result) {
+    if(err) {
+      return console.error('error running query', err);
+    }
+    console.log(result.rows[0]);
+    // >> output: 2018-08-23T14:02:57.117Z
+    client.end();
+  });
+});
 
