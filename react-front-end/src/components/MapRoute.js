99ce5a7ccedbfc75d8dd240b7ce52bed53c494cb
Michelle Tran <michelletraan@hotmail.com>
2020-11-21 19:02:51 +0000

    Attempted geoJSON - not complete

--- react-front-end/src/components/MapRoute.js
+++ react-front-end/src/components/MapRoute.js
@@ -1,17 +1,25 @@
-import React, { useState, useEffect } from "react";
+import React, { componentDidMount, useState, useEffect } from "react";
 import axios from "axios";
-// import { Map } from 'leaflet';
-import { Map, TileLayer, Marker, Popup } from 'react-leaflet'
+import { Map, TileLayer, Marker, Popup, GeoJSON, LayersControl } from 'react-leaflet'
 import 'leaflet/dist/leaflet.css';
+import healthData from "./../data/Health_Region_Summaries.json"
 
 export default function MapRoute() {
+
   return (
     // higher zoom # = more zoomed in 
     <Map center={[58.112709, -103.153648]} zoom={5}>
-      <TileLayer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-      />
+      <LayersControl position="topright">
+        <LayersControl.BaseLayer checked name="OpenStreetMap.Mapnik">
+          <TileLayer
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          />
+        </LayersControl.BaseLayer>
+        <LayersControl.AddOverlay name="Health units">
+          <GeoJSON data={healthData.features} />
+        </LayersControl.AddOverlay>
+      </LayersControl>
     </Map>
   );
 }
