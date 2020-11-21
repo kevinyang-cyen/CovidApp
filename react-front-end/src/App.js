5572e68116d4371e764716c25285dbb21286ef0d
Kevin Yang <kevinyang.cyen@gmail.com>
2020-11-20 17:32:02 -0800

    created all the paths

--- react-front-end/src/App.js
+++ react-front-end/src/App.js
@@ -3,14 +3,25 @@ import './App.css';
 // import { useState, useEffect } from "react";
 // import axios from "axios";
 import { Route, BrowserRouter as Router } from "react-router-dom";
-import Map from './components/Map.js';
+import Dashboard from './components/Dashboard';
+import Map from './components/Map';
+import Assessment from './components/Assessment';
+import News from './components/News';
+import Login from './components/Login';
+import Register from './components/Register';
+
 
 
 function App() {
 
   return (
     <Router>
-      <Route path='/map' component={Map} />
+      <Route path='/' exact component={Dashboard} />
+      <Route path='/map' exact component={Map} />
+      <Route path='/assessment' exact component={Assessment} />
+      <Route path='/news' exact component={News} />
+      <Route path='/login' exact component={Login} />
+      <Route path='/Register' exact component={Register} />
     </Router>
   );
 }
---
+++ react-front-end/src/components/Assessment.js
@@ -0,0 +1,19 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+export default function Assessment() {
+  const [testVar, setTestVar] = useState('Kevin');
+
+  useEffect(() => {
+
+    axios.get("http://localhost:8080/map")
+    .then((res) => {
+      console.log(res.data);
+      setTestVar(res.data);
+     })
+
+  }, []);
+  return (
+    <h1>{testVar}</h1>
+  );
+}
