0724f24dfcd6cbc94c522dba9dc82dabbe689a30
Michelle Tran <michelletraan@hotmail.com>
2020-11-21 16:25:40 -0800

    Testing - does not work still :)

--- express-back-end/routes/map.js
+++ express-back-end/routes/map.js
@@ -1,12 +1,21 @@
+import healthRegion from "../Health_Region_Summaries.json"
+const L = require('leaflet');
+
 // Map Route
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
 
 
 module.exports = () => {
 
   router.get("/", (req, res) => {
-    res.send('testing testing');
+    const map = L.map('map').setView([43.659752, -79.378161], 20);
+    L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="https://carto.com/attributions">CARTO</a>',
+      subdomains: 'abcd',
+      maxZoom: 21
+    }).addTo(map);
+    res.send(healthRegion);
   });
 
 
--- react-front-end/src/components/CovidMap.js
+++ react-front-end/src/components/CovidMap.js
@@ -1,38 +1,28 @@
 import { MapContainer, TileLayer, GeoJSON } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import healthRegion from "../data/health.json";
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 export default function CovidMap() {
-  const [testRegion, setTestRegion] = useState(healthRegion);
-  console.log(testRegion)
-  // useEffect(() => {
-  //   axios.get("http://localhost:8080/map").then((res) => {
-  //     console.log(res.data);
-  //     setTestRegion(res.data);
-  //   });
-  // }, []);
-  // console.log(testRegion)
+  console.log('rendering')
   return (
     <MapContainer
       style={{ height: "100vh" }}
       zoom={2}
       center={[56.130367, -106.346771]}
     >
-      {console.log(testRegion)}
+      <GeoJSON
+        data={healthRegion}
+        style={() => ({
+          color: "#4a83ec",
+          weight: 2,
+          fillColor: "transparent",
+        })}
+      />
       <TileLayer
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      <GeoJSON
-        data={testRegion}
-        style={() => ({
-          color: "#4a83ec",
-          weight: 0.3,
-          fillColor: "#1a1d62",
-          fillOpacity: 0.3,
-        })}
       />
     </MapContainer>
   );
