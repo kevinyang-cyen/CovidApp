ea766d45a23eb229d6341e7d0a1cd5c183fcf606
Kevin Yang <kevinyang.cyen@gmail.com>
2020-11-30 11:33:10 -0800

    initial deploy for heroku

--- express-back-end/server.js
+++ express-back-end/server.js
@@ -1,14 +1,14 @@
 const Express = require('express');
 const App = Express();
 const BodyParser = require('body-parser');
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 require('dotenv').config('/.env');
 const cors = require('cors');
 
 // Express Configuration
 App.use(BodyParser.urlencoded({ extended: false }));
 App.use(BodyParser.json());
-App.use(Express.static('public'));
+App.use(Express.static('../react-front-end/build'));
 App.use(cors());
 
 // Routes Import
