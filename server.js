fb6b22de1030e6de1319aa1ac2e5cd9ce7f6d9a8
Kevin Yang <kevinyang.cyen@gmail.com>
2020-11-30 17:06:27 -0800

    re-saving map data every 24 hours

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const BodyParser = require('body-parser');
 const PORT = process.env.PORT || 8080;
 require('dotenv').config('/.env');
 const cors = require('cors');
+const https = require('https');
+const fs = require('fs');
+const axios = require("axios");
 
 // Express Configuration
 App.use(BodyParser.urlencoded({ extended: false }));
@@ -49,6 +52,25 @@ App.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`Express seems to be listening on port ${PORT} so that's pretty good 👍`);
 });
+
+axios.get("https://services9.arcgis.com/pJENMVYPQqZZe20v/arcgis/rest/services/NewHybridRegionalHeathBoundaries/FeatureServer/0/query?where=1%3D1&outFields=*&outSR=4326&f=geojson")
+.then( (response) => {
+  fs.writeFile('./react-front-end/src/data/health.json', JSON.stringify(response.data, null, 2), function (err) {
+    if (err) throw err;
+    console.log('Saved!');
+  });  
+}  
+)
+
+setInterval(function() {
+  https.get("https://services9.arcgis.com/pJENMVYPQqZZe20v/arcgis/rest/services/NewHybridRegionalHeathBoundaries/FeatureServer/0/query?where=1%3D1&outFields=*&outSR=4326&f=json", (response) => {
+    fs.writeFile('./react-front-end/src/data/health.json', JSON.stringify(response.data, null, 2), function (err) {
+      if (err) throw err;
+      console.log('Saved!');
+    });    
+    }
+  )
+}, 86400000)
 
 
 
